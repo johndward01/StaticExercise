6d1ea565a730b056747b348ca849303f70311e66
johnd <johndward01@yahoo.com>
2020-07-06 16:06:10 -0500

    Initial commit

---
+++ StaticExcercise/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace StaticExcercise
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(TempConverter.FahrenheitToCelcius(32));
+
+            Console.WriteLine(TempConverter.CelciusToFahrenheit(0));
+
+            Console.WriteLine(TempConverter.CelciusToFahrenheit(100));
+
+            Console.WriteLine(TempConverter.FahrenheitToCelcius(212));
+
+
+        }
+    }
+}
---
+++ StaticExcercise/TempConverter.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace StaticExcercise
+{
+    static class TempConverter
+    {
+        public static double F = 0;
+        public static double C = 0;
+        public static double FahrenheitToCelcius(double fTemp)
+        {
+            C = (fTemp - 32) / 1.8000;
+            return C;
+        }
+
+        public static double CelciusToFahrenheit(double cTemp)
+        {
+            F = (cTemp * (5 / 9)) + 32;
+            return F;
+        }
+    }
+}
+
